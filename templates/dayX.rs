f2bd490ba50535746551e88960321f89d6f5bb15
A-725-K <andrea.canepa.12@gmail.com>
2025-11-24 15:43:41 +0100

    Improve Rust project structure

--- templates/dayX.rs
+++ templates/dayX.rs
@@ -1,20 +1,7 @@
-use std::{env, fs::read_to_string};
-
-fn parse_input(filename: &str) -> Vec<String> {
-    read_to_string(filename)
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
+pub fn part1(lines: &Vec<String>, day: usize) {
+    println!("Day {day}, part 1");
 }
 
-fn part1(lines: &Vec<String>) {}
-
-fn part2(lines: &Vec<String>) {}
-
-pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    let lines = parse_input(&args[1]);
-    part1(&lines);
-    part2(&lines);
+pub fn part2(lines: &Vec<String>, day: usize) {
+    println!("Day {day}, part 2");
 }
